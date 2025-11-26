875a7a5a8eb6eba58f0d7358e94f4ed9f0503821
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2025-11-26 21:27:45 +0100

    feat: validator

---
+++ ParkingApi/src/Application/Parking/Commands/ExitParkingCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkingApi.Application.Parking.Commands;
+public class ExitParkingCommandValidator : AbstractValidator<ExitParkingCommand>
+{
+    public ExitParkingCommandValidator()
+    {
+        RuleFor(v => v.VehicleReg)
+            .NotEmpty()
+            .MaximumLength(200)
+                .WithMessage("'{PropertyName}' must be provided.");
+    }
+}
